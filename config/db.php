419ab3940ebe8d8aab0ae301ce4af6fd3984c44f
Gennady Tatarnikov <gtatarnikov@sistema-v.ru>
2020-10-19 19:46:47 +0700

    start Yii2 basic

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'pgsql:host=localhost;port=5433;dbname=test',
+    'username' => 'postgres',
+    'password' => '12345678',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
--- config/web.php
+++ config/web.php
@@ -7,6 +7,8 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'language' => 'ru',
+    'defaultRoute' => 'site/index',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
@@ -14,7 +16,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'e6ddc3b8167f7d912e74ab5aef204b706ac4c738',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -43,14 +45,13 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '<module:\w+>/<controller:\w+>/<action:\w+>' => '<module>/<controller>/<action>',
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
